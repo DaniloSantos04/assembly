54fa0afdda281f6aab0db75090fe95534acd76a8
Danilo Santos <santos.danilo@outlook.com>
2023-05-27 14:35:12 -0300

    project initialization

---
+++ src/test/java/br/com/assembly/AssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.assembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
