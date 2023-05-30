33ecbe66a13cc9d0fa023fba7f2dc844f509a233
Danilo Santos <santos.danilo@outlook.com>
2023-05-29 23:49:20 -0300

    Creating a task to close an open session for voting.

---
+++ src/main/java/br/com/assembly/task/MinuteTask.java
@@ -0,0 +1,18 @@
+package br.com.assembly.task;
+
+import br.com.assembly.service.VotingSessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MinuteTask {
+
+    @Autowired
+    private VotingSessionService votingSessionService;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void executeMinuteTask() {
+        votingSessionService.closedVotingSession();
+    }
+}
